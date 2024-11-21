bce47027643e3285d31a0a69fd1d451706e508ab
adarsh251 <mishraadarsh25104@gmail.com>
2024-11-22 03:53:42 +0530

    add favicon

--- src/Components/Blog/NewBlog.jsx
+++ src/Components/Blog/NewBlog.jsx
@@ -174,7 +174,7 @@ const NewBlog = () => {
                 [{ 'header': '1'}, {'header': '2'}],
                 ['bold', 'italic', 'underline', 'strike'],
                 [{'list': 'ordered'}, {'list': 'bullet'}],
-                ['link', 'image'],
+                ['link'],
                 ['clean']
               ]
             }}
